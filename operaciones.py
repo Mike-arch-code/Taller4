3ba6f4f638627f79ac5ddebe2fd3a34ac396a94f
Mike-arch-code <mfigueroah@unal.edu.co>
2024-07-31 10:05:06 -0500

    v2
    producto punto

--- operaciones.py
+++ operaciones.py
@@ -1,22 +1,34 @@
-def promedio():
-    numeros_conjunto = [None] * 8
-    i = 0
-    sum  = 0
-    while i < 8:
+def producto_punto():
+
+    while True:
         try:
-            numeros_conjunto[i] = float(input("Dijite 1 numero: "))
-            sum = sum + numeros_conjunto[i]
+            n = int(input("Dijite la catidad de  n numeros para los conjuntos:"))
+            break
+        except:
+            print("El valor ingresado no es un número entero. Intente de nuevo.")
+
+    v = [None] * n
+    w = [None] * n
+    
+    i = 0
+    while i < n:
+        try:
+            v[i] = float(input("Dijite 1 numero para el conjunto v :"))
+            i += 1
+        except ValueError:
+            print("El valor ingresado no es un número real. Intente de nuevo.")
+    i = 0
+    while i < n:
+        try:
+            w[i] = float(input("Dijite 1 numero para el conjunto w :"))
             i += 1
         except ValueError:
             print("El valor ingresado no es un número real. Intente de nuevo.")
 
-    print("El conjunto de numeros es:",numeros_conjunto)
 
-    promedio_numeros = sum / 8 
+    producto = sum(a * b for a, b in zip(v, w))
 
-    print("El promedio es:",promedio_numeros)
+    print("Conjunto v:",v)
+    print("Conjunto w:",w)
 
-    
-    
-
-    
+    print("El producto punto de los vectores es:",producto)
